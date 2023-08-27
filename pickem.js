69f5a592fabd7b068f14ca97152b8991d3a6a2ac
Gabriel Baduqui <baduquig@gmail.com>
2023-08-27 09:05:37 -0700

    pickem.js WIP...

--- pickem.js
+++ pickem.js
@@ -1,4 +1,13 @@
 const currentTimestamp = new Date();
+const urlParams = new URLSearchParams(window.location.search);
+const userID = urlParams.get('userID');
+const gameWeeks = [];
+const firstGameday = new Date('2023-08-26');
+
+for (i = 0; i < 16; i++) {
+    //const nextDate = firstGameday + (7 * i);
+    const nextDate = firstGameday.setDate(firstGameday.getDate() + (7 * i));
+}
 
 // Instantiage All Data //
 fetch('http://gbaduqui.pythonanywhere.com/alldata')
@@ -9,8 +18,14 @@ fetch('http://gbaduqui.pythonanywhere.com/alldata')
         return response.json();
     })
     .then(data => {
+        /*******************/
         const allData = data;
+        const userPicks = allData.filter(pick => {
+            return pick.userID == id
+        });
+        /*******************/
         console.log(allData);
+        console.log(userPicks);
     })
     .catch(error => {
         console.log(error);
