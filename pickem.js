fd08a52d9b720de525f05cb0c8e2e7998fadffc7
Gabriel Baduqui <baduquig@gmail.com>
2023-09-03 07:20:27 -0700

    instantiate user dropdown

--- pickem.js
+++ pickem.js
@@ -1,15 +1,18 @@
 const currentTimestamp = new Date();
 const urlParams = new URLSearchParams(window.location.search);
 const userID = urlParams.get('userID');
+const allData = [];
 const weekDropdown = document.getElementById('pickem-week');
+const userDropdown = document.getElementById('pickem-user');
 const gameWeeks = [];
 const nextGameday = new Date('2023-08-26');
 let currentWeek = 0;
 let weekOptions = '';
+let userOptions = '';
 
 const setWeekdropdown = () => {
     // Set Week Dropdown //
-    for (i = 0; i < 15; i ++) {
+    for (let i = 0; i < 15; i ++) {
         weekOptions += '<option value="' + i + '">' + i + '</option>';
     }
     weekDropdown.innerHTML = weekOptions;
@@ -18,6 +21,20 @@ const setWeekdropdown = () => {
         currentWeek ++;
         weekDropdown.value = currentWeek;
     }
+}
+
+const setUserDropdown = () => {
+    const distinctUsersSet = new Set();
+    for (let j = 0; j < allData.length; j++) {
+        distinctUsersSet.add({
+            id: allData[j].userID,
+            userName: allData[j].userName
+        });
+    }
+    for (let user of distinctUsersSet) {
+        userOptions += '<option value="' + user.id + '">' + user.userName + '</option>';
+    }
+    userDropdown.innerHTML = userOptions;    
 }
 
 // Instantiage All Data //
@@ -29,18 +46,18 @@ fetch('http://gbaduqui.pythonanywhere.com/alldata')
         return response.json();
     })
     .then(data => {
-        /*******************/
-        const allData = data;
+        allData = data;
+        /*******************
         const userPicks = allData.filter(pick => {
             return pick.userID == id
         });
+        console.log(userPicks);
         /*******************/
         console.log(allData);
-        console.log(userPicks);
+        setWeekdropdown();
+        setUserDropdown();
     })
     .catch(error => {
         console.log(error);
     });
 
-// Set Week Dropdown //
-setWeekdropdown();
